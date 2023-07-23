cf504f500620707f334a8575b1c0f9237959738b
Eunbi Kang <jenabill@naver.com>
2023-07-23 12:53:25 +0900

    add room decoration page

--- src/components/FurnitureList/index.js
+++ src/components/FurnitureList/index.js
@@ -1,13 +1,44 @@
+import { useSetRecoilState } from "recoil";
 import CommonItem from "../CommonItem";
+import { roomFurnitureListState } from "../../store/furniture";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const FurnitureList= () => {
-    const list = Array(8).fill("");
+const FurnitureList = ({ user }) => {
+    const [list, setList] = useState([]);
+    const setRoomFurnitureList = useSetRecoilState(roomFurnitureListState);
+    const onClick = (itemImagePath, furnitureId) => () => {
+        setRoomFurnitureList((state) => ({
+            nextId: state.nextId + 1,
+            list: state.list.concat([{
+                id: state.nextId,
+                imagePath: itemImagePath,
+                furnitureId,
+                x: 0,
+                y: 0
+            }])
+        }))
+    }
+    useEffect(() => {
+        axios.post("/api/furniture/getMyFurniture", {
+            userId: user._id
+        }).then(({ data }) => {
+            setList(data);
+            console.log(data)
+        }).catch((e) => {
+            console.log(e)
+        })
+    }, [])
     return (
         <div className="white-box">
             <h2 className="white-box-title">내 가구</h2>
-            <ul className="item-list">
-                {list.map(item => <CommonItem name={"편안한 의자"} itemImagePath={"/images/furniture.svg"} />)}
-            </ul>
+            {list.length === 0 ? (
+                <p className="empty-text">내 가구가 없습니다.</p>
+            ) : (
+                <ul className="item-list">
+                    {list.map(item => <CommonItem key={item._id} name={item.name} itemImagePath={item.imagePath} onClickItem={onClick(item.imagePath, item._id)} />)}
+                </ul>
+            )}
         </div>
     )
 }
---
+++ src/components/RoomCanvas/index.js
@@ -0,0 +1,105 @@
+import { useEffect, useRef, useState } from 'react';
+import { Stage, Layer, Image } from 'react-konva';
+import { useRecoilState } from 'recoil';
+import { roomFurnitureListState } from '../../store/furniture';
+import axios from 'axios';
+
+const RoomCanvas = ({ roomId }) => {
+    const [{ list: roomFurnitureList }, setRoomFurnitureList] = useRecoilState(roomFurnitureListState);
+    const stage = useRef(null);
+    const [size, setSize] = useState({
+        width: 0,
+        height: 0
+    })
+    const resizeCanvas = (canvas) => {
+        setSize({
+            width: canvas.parentElement.clientWidth - 10,
+            height: canvas.parentElement.clientHeight - 31
+        })
+    }
+    const onDragStart = (id) => (e) => {
+        const list = roomFurnitureList.slice();
+        const item = list.find((item) => item.id === id);
+        console.log(item);
+        const index = list.indexOf(item);
+        list.splice(index, 1);
+        list.push(item);
+        setRoomFurnitureList((state) => ({
+            ...state,
+            list
+        }));
+    }
+    const onDragEnd = (id) => (e) => {
+        const list = roomFurnitureList.slice();
+        const item = list.find((i) => i.id === id);
+        const index = list.indexOf(item);
+        // update item position
+        list[index] = {
+          ...item,
+          x: e.target.x(),
+          y: e.target.y(),
+        };
+        setRoomFurnitureList((state) => ({
+            ...state,
+            list
+        }))
+    }
+    const onDoubleClick = (id) => (e) => {
+        setRoomFurnitureList((state) => ({
+            ...state,
+            list: state.list.filter((item) => item.id !== id)
+        }))
+    }
+    useEffect(() => {
+        const canvas = document.querySelector("#myCanvas");
+        if (canvas === null) return;
+        resizeCanvas(canvas);
+        const onResize = () => resizeCanvas(canvas);
+        window.addEventListener("resize", onResize);
+        return () => {
+            window.removeEventListener("resize", onResize)
+        }
+    }, []);
+    useEffect(() => {
+        if (roomId !== null) {
+            axios.post("/api/furniture/getFurniturePosition", {
+                roomId
+            }).then(({ data }) => {
+                console.log(data);
+                setRoomFurnitureList({
+                    nextId: 1,
+                    list: data.map((item) => ({
+                        ...item,
+                        id: item.roomFurnitureId
+                    }))
+                })
+            }).catch((e) => {
+                console.log(e);
+            })
+        }
+    }, [roomId])
+    return (
+        <Stage width={size.width} height={size.height} id='myCanvas' ref={stage}>
+            <Layer>
+            {roomFurnitureList.map(furniture => {
+                const image = new window.Image();
+                image.src = furniture.imagePath;
+                return (
+                    <Image
+                        key={furniture.id}
+                        draggable
+                        image={image} 
+                        x={furniture.x} 
+                        y={furniture.y}
+                        onDragStart={onDragStart(furniture.id)}
+                        onDragEnd={onDragEnd(furniture.id)}
+                        onDblClick={onDoubleClick(furniture.id)}
+                    />
+                )
+            })}
+            </Layer>
+      </Stage>
+    )
+}
+
+export default RoomCanvas;
