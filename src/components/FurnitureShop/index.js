5e2f31162029c77fd9b638ea6352e0be10a380ff
Eunbi Kang <jenabill@naver.com>
2023-07-25 11:39:21 +0900

    update furniture shop and list component

--- src/components/FurnitureShop/index.js
+++ src/components/FurnitureShop/index.js
@@ -1,21 +1,28 @@
 import { useEffect, useState } from "react";
 import CommonItem from "../CommonItem";
 import Skeleton from "../Skeleton";
+import FurnitureCategory from "../FurnitureCategory";
 import axios from "axios";
 
 const FurnitureShop = ({ user }) => {
     const [loading, setLoading] = useState(true);
     const [list, setList] = useState([])
+    const [selected, setSelected] = useState(0);
     useEffect(() => {
+        setLoading(true);
         axios.post("/api/furniture/getFurniture", {
-            userId: user._id
+            userId: user._id,
+            category: selected
         }).then(({ data }) => {
             setList(data)
-            setLoading(false)
+            setLoading(false);
         }).catch((e) => {
             console.log(e);
         });
-    }, [])
+    }, [selected])
+    const onClickCategory = (index) => () => {
+        setSelected(index);
+    }
     const onClickItem = (name, furnitureId) => async () => {
         try {
             if (window.confirm(`${name}을(를) 구매하시겠습니까?`)) {
@@ -37,6 +44,7 @@ const FurnitureShop = ({ user }) => {
         return (
             <div className="white-box">
                 <h2 className="white-box-title">상점</h2>
+                <FurnitureCategory onClick={onClickCategory} selected={selected} />
                 <ul className="item-list">
                     {Array(3).fill("").map(() => <Skeleton width="100%" height="150px" />)}
                 </ul>
@@ -46,9 +54,12 @@ const FurnitureShop = ({ user }) => {
     return (
         <div className="white-box">
             <h2 className="white-box-title">상점</h2>
-            <ul className="item-list">
-                {list.map(item => <CommonItem name={item.name} price={item.price} itemImagePath={item.imagePath} onClickItem={onClickItem(item.name, item._id)} />)}
-            </ul>
+            <FurnitureCategory onClick={onClickCategory} selected={selected} />
+            {list.length === 0 ? <p className="empty-text">가구가 없습니다.</p> : (
+                <ul className="item-list">
+                    {list.map(item => <CommonItem name={item.name} price={item.price} itemImagePath={item.imagePath} onClickItem={onClickItem(item.name, item._id)} />)}
+                </ul>
+            )}
         </div>
     )
 }
